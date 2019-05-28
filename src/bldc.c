713af66bc1c3a4433d0512015760f587fdaef417
lalalandrus <lalalandrus@hotmail.com>
2019-05-28 15:03:07 -0700

    new motor controller

--- src/bldc.c
+++ src/bldc.c
@@ -3,6 +3,9 @@
 #include "defines.h"
 #include "setup.h"
 #include "config.h"
+
+// Matlab includes
+#include "BLDC_controller.h"           /* Model's header file */
 #include "bldc.h"
 #include "hallinterrupts.h"
 
@@ -10,13 +13,8 @@ volatile ELECTRICAL_PARAMS electrical_measurements;
 
 #define DO_MEASUREMENTS
 
-
-volatile int posl = 0;
-volatile int posr = 0;
 volatile int pwml = 0;
 volatile int pwmr = 0;
-volatile int weakl = 0;
-volatile int weakr = 0;
 
 extern volatile int speed;
 
@@ -25,139 +23,13 @@ extern volatile adc_buf_t adc_buffer;
 extern volatile uint32_t timeout;
 extern uint8_t disablepoweroff;
 
-uint32_t buzzerFreq = 0;
-uint32_t buzzerPattern = 0;
+uint32_t buzzerFreq     = 0;
+uint32_t buzzerPattern  = 0;
+uint32_t buzzerTimer    = 0;
 
-uint8_t enable = 0;
+uint8_t enable          = 0;
 
-const int pwm_res = 64000000 / 2 / PWM_FREQ; // = 2000
-
-const uint8_t hall_to_pos[8] = {
-    0,
-    0,
-    2,
-    1,
-    4,
-    5,
-    3,
-    0,
-};
-
-/*
-static const float DUTY_SINUSOIDAL[6][16]= {
-		{0.86328125, 0.89453125, 0.921875, 0.9453125, 0.96484375, 0.9765625, 0.98828125, 0.99609375,	0.99609375, 0.99609375, 0.98828125, 0.98046875, 0.96484375, 0.9453125, 0.92578125, 0.8984375},
-		{0.8671875, 0.890625, 0.91796875, 0.94140625, 0.9609375, 0.9765625, 0.98828125, 0.99609375,	0.99609375, 0.99609375, 0.9921875, 0.98046875, 0.96484375, 0.94921875, 0.92578125, 0.8984375},
-		{0.87109375, 0.8359375, 0.796875, 0.7578125, 0.71484375, 0.66796875, 0.6171875, 0.5625,	0.5078125, 0.453125, 0.39453125, 0.33203125, 0.26953125, 0.20703125, 0.140625, 0.078125},
-		{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,	0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-		{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,	0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-		{0.0078125, 0.07421875, 0.13671875, 0.203125, 0.265625, 0.328125, 0.38671875, 0.44921875,0.49609375, 0.5546875, 0.60546875, 0.65625, 0.703125, 0.75, 0.7890625, 0.828125}
-};
-*/
-
-static const int DUTY_SINUSOIDAL_INT[6][16]= {
-		{86, 89, 92, 95, 96, 98, 99, 100,	100, 100, 99, 98, 96, 95, 93, 90},
-		{87, 89, 92, 94, 96, 98, 99, 100,	100, 100, 99, 98, 96, 95, 93, 90},
-		{87, 84, 80, 76, 71, 67, 62, 56,	50, 45, 39, 33, 27, 21, 14, 8},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{1, 7, 14, 20, 27, 33, 39, 45, 50, 55, 61, 66, 70, 75, 79, 83}
-};
-
-int sign[6] = {1, 1, 0, -1, -1, 0};
-
-static inline void blockPWMSin(int pwm, int pos, int fraction, int *u, int *v, int *w) {
-  *u = (pwm*(DUTY_SINUSOIDAL_INT[(pos+2)%6][fraction]-50))/50;
-  *v = (pwm*(DUTY_SINUSOIDAL_INT[(pos+0)%6][fraction]-50))/50;
-  *w = (pwm*(DUTY_SINUSOIDAL_INT[(pos+4)%6][fraction]-50))/50;
-}
-
-
-static inline void blockPWM(int pwm, int pos, int *u, int *v, int *w) {
-  switch(pos) {
-    case 0:
-      *u = 0;
-      *v = pwm;
-      *w = -pwm;
-      break;
-    case 1:
-      *u = -pwm;
-      *v = pwm;
-      *w = 0;
-      break;
-    case 2:
-      *u = -pwm;
-      *v = 0;
-      *w = pwm;
-      break;
-    case 3:
-      *u = 0;
-      *v = -pwm;
-      *w = pwm;
-      break;
-    case 4:
-      *u = pwm;
-      *v = -pwm;
-      *w = 0;
-      break;
-    case 5:
-      *u = pwm;
-      *v = 0;
-      *w = -pwm;
-      break;
-    default:
-      *u = 0;
-      *v = 0;
-      *w = 0;
-  }
-}
-
-static inline void blockPhaseCurrent(int pos, int u, int v, int *q) {
-  switch(pos) {
-    case 0:
-      *q = u - v;
-      // *u = 0;
-      // *v = pwm;
-      // *w = -pwm;
-      break;
-    case 1:
-      *q = u;
-      // *u = -pwm;
-      // *v = pwm;
-      // *w = 0;
-      break;
-    case 2:
-      *q = u;
-      // *u = -pwm;
-      // *v = 0;
-      // *w = pwm;
-      break;
-    case 3:
-      *q = v;
-      // *u = 0;
-      // *v = -pwm;
-      // *w = pwm;
-      break;
-    case 4:
-      *q = v;
-      // *u = pwm;
-      // *v = -pwm;
-      // *w = 0;
-      break;
-    case 5:
-      *q = -(u - v);
-      // *u = pwm;
-      // *v = 0;
-      // *w = -pwm;
-      break;
-    default:
-      *q = 0;
-      // *u = 0;
-      // *v = 0;
-      // *w = 0;
-  }
-}
-
-uint32_t buzzerTimer        = 0;
+const int pwm_res       = 64000000 / 2 / PWM_FREQ; // = 2000
 
 int offsetcount = 0;
 int offsetrl1   = 2000;
@@ -170,62 +42,10 @@ int offsetdcr   = 2000;
 float batteryVoltage = BAT_NUMBER_OF_CELLS * 4.0;
 
 int curl = 0;
-// int errorl = 0;
-// int kp = 5;
-// volatile int cmdl = 0;
-
-int last_pos = 0;
-int timer = 0;
-const int max_time = PWM_FREQ / 10;
-volatile int vel = 0;
-
-volatile int counts_to_change = 0;
-
-#ifdef HALL_INTERRUPTS
-extern long long now_us;
-#endif
-
-
 //scan 8 channels with 2ADCs @ 20 clk cycles per sample
 //meaning ~80 ADC clock cycles @ 8MHz until new DMA interrupt =~ 100KHz
 //=640 cpu cycles
-// Careful - easy to use too many!
 void DMA1_Channel1_IRQHandler() {
-  __disable_irq(); // but we want both values at the same time, without interferance
-#ifdef HALL_INTERRUPTS
-  unsigned long time = h_timer_hall.Instance->CNT;
-  long long timerwraps_copy = timerwraps;
-  now_us = ((timerwraps_copy<<16) + time) * 10;
-#endif
-  unsigned char hall[2];
-  hall[0] = (~(LEFT_HALL_U_PORT->IDR & (LEFT_HALL_U_PIN | LEFT_HALL_V_PIN | LEFT_HALL_W_PIN))/LEFT_HALL_U_PIN) & 7;
-  hall[1] = (~(RIGHT_HALL_U_PORT->IDR & (RIGHT_HALL_U_PIN | RIGHT_HALL_V_PIN | RIGHT_HALL_W_PIN))/RIGHT_HALL_U_PIN) & 7;
-  __enable_irq();
-
-
-  // ok, so that last transtion from one hall sector tyo the next took
-  //HallData[i].HallTimeDiff
-  // so we assume this one will...
-  // and use this to chose one of 16 phases
-  // which we'll modulate the pwm with?
-#ifdef HALL_INTERRUPTS
-  long long timenow = (timerwraps_copy << 16) | time;
-  char fraction[2];
-  for (int i = 0; i < 2; i++){
-    long long dt = timenow - local_hall_params[i].last_hall_time;
-
-    local_hall_params[i].dmacount -= dt/10;
-    if (dt > local_hall_params[i].dmacount)
-      local_hall_params[i].dmacount = dt;
-
-    dt *= 16;
-    int dta = dt/HallData[i].HallTimeDiff;
-
-    if (dta < 0) dta = 0;
-    fraction[i] = dta;
-    if (fraction[i] > 15) fraction[i] = 15;
-  }
-#endif
 
   DMA1->IFCR = DMA_IFCR_CTCIF1;
   // HAL_GPIO_WritePin(LED_PORT, LED_PIN, 1);
@@ -281,14 +101,6 @@ void DMA1_Channel1_IRQHandler() {
     RIGHT_TIM->BDTR |= TIM_BDTR_MOE;
   }
 
-  int ul, vl, wl;
-  int ur, vr, wr;
-
-  posl = (hall_to_pos[hall[0]] + 2) % 6;
-  posr = (hall_to_pos[hall[1]] + 2) % 6;
-
-  blockPhaseCurrent(posl, adc_buffer.rl1 - offsetrl1, adc_buffer.rl2 - offsetrl2, &curl);
-
 #ifdef DO_MEASUREMENTS
   electrical_measurements.motors[0].r1 = adc_buffer.rl1 - offsetrl1;
   electrical_measurements.motors[0].r2 = adc_buffer.rl2 - offsetrl2;
@@ -309,43 +121,64 @@ void DMA1_Channel1_IRQHandler() {
       HAL_GPIO_WritePin(BUZZER_PORT, BUZZER_PIN, 0);
   }
 
-  //update PWM channels based on position
-  if (0){
-  #ifdef HALL_INTERRUPTS
-    blockPWMSin(pwml, posl, fraction[0], &ul, &vl, &wl);
-    blockPWMSin(pwmr, posr, fraction[1], &ur, &vr, &wr);
-  #endif
-  } else {
-    blockPWM(pwml, posl, &ul, &vl, &wl);
-    blockPWM(pwmr, posr, &ur, &vr, &wr);
+  // ############################### MOTOR CONTROL ###############################
+  int ul, vl, wl;
+  int ur, vr, wr;
+
+  // Get hall sensors values
+  uint8_t hall_ul = !(LEFT_HALL_U_PORT->IDR & LEFT_HALL_U_PIN);
+  uint8_t hall_vl = !(LEFT_HALL_V_PORT->IDR & LEFT_HALL_V_PIN);
+  uint8_t hall_wl = !(LEFT_HALL_W_PORT->IDR & LEFT_HALL_W_PIN);
+
+  uint8_t hall_ur = !(RIGHT_HALL_U_PORT->IDR & RIGHT_HALL_U_PIN);
+  uint8_t hall_vr = !(RIGHT_HALL_V_PORT->IDR & RIGHT_HALL_V_PIN);
+  uint8_t hall_wr = !(RIGHT_HALL_W_PORT->IDR & RIGHT_HALL_W_PIN);
+
+  static boolean_T OverrunFlag = false;
+  /* Check for overrun */
+  if (OverrunFlag) {
+    return;
   }
+  OverrunFlag = true;
+ 
+    /* Set motor inputs here */
+    rtU.b_hallALeft   = hall_ul;
+    rtU.b_hallBLeft   = hall_vl;
+    rtU.b_hallCLeft   = hall_wl;
+    rtU.r_DCLeft      = pwml;
+
+    rtU.b_hallARight  = hall_ur;
+    rtU.b_hallBRight  = hall_vr;
+    rtU.b_hallCRight  = hall_wr;
+    rtU.r_DCRight     = pwmr;
+
+    /* Step the controller */
+    BLDC_controller_step();
+
+    /* Get motor outputs here */
+    ul            = rtY.DC_phaALeft;
+    vl            = rtY.DC_phaBLeft;
+    wl            = rtY.DC_phaCLeft;
+  // motSpeedLeft = rtY.n_motLeft;
+  // motAngleLeft = rtY.a_elecAngleLeft;
+
+    ur            = rtY.DC_phaARight;
+    vr            = rtY.DC_phaBRight;
+    wr            = rtY.DC_phaCRight;
+ // motSpeedRight = rtY.n_motRight;
+ // motAngleRight = rtY.a_elecAngleRight;
+
+  /* Indicate task complete */
+  OverrunFlag = false;
+  // ###############################################################################
+
+  LEFT_TIM->LEFT_TIM_U    = CLAMP(ul + pwm_res / 2, 10, pwm_res-10);
+  LEFT_TIM->LEFT_TIM_V    = CLAMP(vl + pwm_res / 2, 10, pwm_res-10);
+  LEFT_TIM->LEFT_TIM_W    = CLAMP(wl + pwm_res / 2, 10, pwm_res-10);
+
+  RIGHT_TIM->RIGHT_TIM_U  = CLAMP(ur + pwm_res / 2, 10, pwm_res-10);
+  RIGHT_TIM->RIGHT_TIM_V  = CLAMP(vr + pwm_res / 2, 10, pwm_res-10);
+  RIGHT_TIM->RIGHT_TIM_W  = CLAMP(wr + pwm_res / 2, 10, pwm_res-10);
 
 
-  int weakul, weakvl, weakwl;
-  if (pwml > 0) {
-    blockPWM(weakl, (posl+5) % 6, &weakul, &weakvl, &weakwl);
-  } else {
-    blockPWM(-weakl, (posl+1) % 6, &weakul, &weakvl, &weakwl);
-  }
-  ul += weakul;
-  vl += weakvl;
-  wl += weakwl;
-
-  int weakur, weakvr, weakwr;
-  if (pwmr > 0) {
-    blockPWM(weakr, (posr+5) % 6, &weakur, &weakvr, &weakwr);
-  } else {
-    blockPWM(-weakr, (posr+1) % 6, &weakur, &weakvr, &weakwr);
-  }
-  ur += weakur;
-  vr += weakvr;
-  wr += weakwr;
-
-  LEFT_TIM->LEFT_TIM_U = CLAMP(ul + pwm_res / 2, 10, pwm_res-10);
-  LEFT_TIM->LEFT_TIM_V = CLAMP(vl + pwm_res / 2, 10, pwm_res-10);
-  LEFT_TIM->LEFT_TIM_W = CLAMP(wl + pwm_res / 2, 10, pwm_res-10);
-
-  RIGHT_TIM->RIGHT_TIM_U = CLAMP(ur + pwm_res / 2, 10, pwm_res-10);
-  RIGHT_TIM->RIGHT_TIM_V = CLAMP(vr + pwm_res / 2, 10, pwm_res-10);
-  RIGHT_TIM->RIGHT_TIM_W = CLAMP(wr + pwm_res / 2, 10, pwm_res-10);
 }
